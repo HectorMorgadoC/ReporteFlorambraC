e3ea10124ebc0d48da19cccf5528eea1dbb12efa
Hector Morgado <e3.corest@outlook.com>
2024-07-21 18:35:32 -0500

    se cambia el idex principal junto as u stilos queda pendiente resolbver el problema de los estilos de los links

--- script.js
+++ script.js
@@ -4,10 +4,19 @@ import { selectData } from "./controllersReports/selectData/selectData.js"
 import { formatDate } from "./controllersReports/formatDate/formatDate.js"
 
 const formData = document.getElementById('formdata')
+const header = document.querySelector('header')
 let data = localStorage.getItem('data')
 
 document.addEventListener('DOMContentLoaded', async () => {
     requestFeth(data,url,selectData)
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) { 
+            header.classList.add('scrolled')
+        } else {
+            header.classList.remove('scrolled')
+        }
+    })
 })
 
 formData.addEventListener('submit',async(e) => {
